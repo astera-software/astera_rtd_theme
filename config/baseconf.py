b67dfe00709cb45da40bc854ac8212520c370754
Faiz Surani <faiz.surani@gmail.com>
2019-08-22 15:39:28 -0700

    Update baseconf.py

--- config/baseconf.py
+++ config/baseconf.py
@@ -58,8 +58,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-master_doc = 'ind'
+html_theme = 'sphinx_rtd_thme'
+master_doc = 'index'
 
 
 html_favicon = '_static/images/favicon.ico'
