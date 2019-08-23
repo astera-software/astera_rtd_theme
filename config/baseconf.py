119fecac1dd2b6906fabf21301d1f0bf1aedeb04
Faiz Surani <faiz.surani@gmail.com>
2019-08-22 22:01:17 -0700

    Update baseconf.py

--- config/baseconf.py
+++ config/baseconf.py
@@ -46,7 +46,7 @@ html_css_files = [
 # defined below (external first) is intentional!
 html_js_files = [
   ('https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js', {'defer': 'defer'}),
-  ('https://raw.githubusercontent.com/astera-software/astera_rtd_theme/master/config/baseconf.py', {'async': 'async'})
+  ('algolia.js', {'async': 'async'})
 ]
 
 # List of patterns, relative to source directory, that match files and
